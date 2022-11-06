a4212cefe3edded53d0cec80bc6e9bc4ce117788
Vladimir Zinchenko <Zincvalo5@gmail.com>
2022-11-07 00:20:22 +0200

    Create TokenResponse

---
+++ src/main/java/com/softserve/itacademy/controller/rest/TokenResponse.java
@@ -0,0 +1,11 @@
+package com.softserve.itacademy.controller.rest;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+    @AllArgsConstructor
+    @Data
+    public class TokenResponse {
+        private String token;
+    }
+
