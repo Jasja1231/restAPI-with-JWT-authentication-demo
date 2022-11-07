1f14c398263695fcb6baba37e51f57ba3858abdf
jasja1231 <jasja1231@gmail.com>
2022-11-07 17:21:15 +0200

    User rest controller dto transformer

---
+++ src/main/java/com/softserve/itacademy/dto/UserDto.java
@@ -0,0 +1,46 @@
+package com.softserve.itacademy.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.softserve.itacademy.model.Role;
+import com.softserve.itacademy.service.UserService;
+import lombok.Data;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+import javax.validation.constraints.NotNull;
+
+@Data //@Getter, @Setter, @ToString, @EqualsAndHashCode, @RequiredArgsConstructor
+public class UserDto {
+
+    public UserDto(long id, String firstName, String lastName, String email, Role role, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.role = role;
+        this.password = password;
+    }
+
+    @NotNull
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Role role;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    @JsonIgnore public String getPassword() {
+        return password;
+    }
+
+    @JsonIgnore public Role getRole() {
+        return role;
+    }
+}
---
+++ src/main/java/com/softserve/itacademy/dto/UserTransformer.java
@@ -0,0 +1,33 @@
+package com.softserve.itacademy.dto;
+
+import com.softserve.itacademy.model.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserTransformer {
+    public static User toUserEntity(UserDto userDto){
+        User user  = new User();
+        user.setId(userDto.getId());
+        user.setFirstName(userDto.getFirstName());
+        user.setLastName(userDto.getLastName());
+        user.setEmail(userDto.getEmail());
+        user.setRole(userDto.getRole());
+        user.setPassword(userDto.getPassword()); //TODO password
+
+        return user;
+    }
+
+
+    public static UserDto toUserDto(User user){
+        return new UserDto(
+                 user.getId()
+                ,user.getFirstName()
+                ,user.getLastName()
+                ,user.getEmail()
+                ,user.getRole()
+                ,user.getPassword()
+        );
+    }
+
+
+}
