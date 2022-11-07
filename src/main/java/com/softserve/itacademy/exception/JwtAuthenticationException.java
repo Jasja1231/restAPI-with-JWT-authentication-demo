4fe181bb713900838352bc64a22fd90235ecc077
Vladimir Zinchenko <Zincvalo5@gmail.com>
2022-11-07 23:33:01 +0200

    Update JwtProvider

---
+++ src/main/java/com/softserve/itacademy/exception/JwtAuthenticationException.java
@@ -0,0 +1,7 @@
+package com.softserve.itacademy.exception;
+
+public class JwtAuthenticationException extends Throwable {
+    public JwtAuthenticationException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/softserve/itacademy/security/JwtProvider.java
+++ src/main/java/com/softserve/itacademy/security/JwtProvider.java
@@ -1,40 +1,102 @@
 package com.softserve.itacademy.security;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import com.softserve.itacademy.exception.JwtAuthenticationException;
+import com.softserve.itacademy.model.Role;
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
 
 @Component
 public class JwtProvider {
+    //    @Value("${security.jwt.token.secret-key}")
+//    private String secretKey;
+//
+//    public String generateToken(String login){
+//        Date date = Date.from(LocalDate.now().plusDays(10).atStartOfDay(ZoneId.systemDefault()).toInstant());
+//        return Jwts.builder()
+//                .setSubject(login)
+//                .setExpiration(date)
+//                .signWith(SignatureAlgorithm.HS512,secretKey)
+//                .compact();
+//    }
+//    public  boolean validateToken(String token){
+//        try {
+//            Jwts.parser().setSigningKey(secretKey).parsePlaintextJws(token);
+//            return true;
+//        }catch (Exception e){
+//            return false;
+//        }
+//    }
+//    public String getLoginFromToken(String token){
+//        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+//
+//    }
+//    public Date getExpirationDateFromToken(String token){
+//        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getExpiration();
+//    }
+//}
     @Value("${security.jwt.token.secret-key}")
-    private String secretKey;
+    private String secret;
 
-    public String generateToken(String login){
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
+    }
+@Autowired
+     private   UserDetailsService userDetailsService;
+
+    public  String createToken(String username, Role role){
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put("role",getRoleName(role));
+        Date now = new Date();
         Date date = Date.from(LocalDate.now().plusDays(10).atStartOfDay(ZoneId.systemDefault()).toInstant());
         return Jwts.builder()
-                .setSubject(login)
+                .setClaims(claims)
+                .setIssuedAt(now)
                 .setExpiration(date)
-                .signWith(SignatureAlgorithm.HS512,secretKey)
+                .signWith(SignatureAlgorithm.HS256,secret)
                 .compact();
     }
-    public  boolean validateToken(String token){
-        try {
-            Jwts.parser().setSigningKey(secretKey).parsePlaintextJws(token);
-            return true;
-        }catch (Exception e){
-            return false;
-        }
-    }
-    public String getLoginFromToken(String token){
-        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
 
+    public Authentication getAuthentication (String token){
+        UserDetails userDetails = this.userDetailsService.loadUserByUsername(token);
+return  new UsernamePasswordAuthenticationToken(userDetails,"",userDetails.getAuthorities());
     }
-    public Date getExpirationDateFromToken(String token){
-        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getExpiration();
+
+    public  String getUsername (String token){
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();
     }
+    public String resolveToken(HttpServletRequest request){
+        String bearerToken = request.getHeader("Authorization");
+        if(bearerToken != null && bearerToken.startsWith("Bearer_")){
+            return  bearerToken.substring(7,bearerToken.length());
+        }
+        return null;
+    }
+    public  boolean validateToken(String token) throws JwtAuthenticationException {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+            if(claims.getBody().getExpiration().before(new Date())){
+                return false;
+            }
+            return  true;
+        }catch (JwtException|IllegalArgumentException e){
+          throw new JwtAuthenticationException("Jwt taken is expired or invalid");
+    }
+}
+private String getRoleName (Role accountRole){return accountRole.getName();}
 }
