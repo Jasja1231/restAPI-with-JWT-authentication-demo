f3ed439f730d1e817c0237f400ada52da257a247
jasja1231 <jasja1231@gmail.com>
2022-11-08 03:11:48 +0200

    new class : JwtUserDetailServiceImpl

---
+++ src/main/java/com/softserve/itacademy/service/detail/JwtUserDetailServiceImpl.java
@@ -0,0 +1,38 @@
+package com.softserve.itacademy.service.detail;
+
+import com.softserve.itacademy.model.User;
+import com.softserve.itacademy.repository.UserRepository;
+import com.softserve.itacademy.security.JwtUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+public class JwtUserDetailServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public JwtUserDetailServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username/*email*/) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByEmail(username);
+        if (user.isEmpty()) throw new UsernameNotFoundException("User not found!");
+
+        //Read user
+        User readUser = user.get();
+        JwtUser jwtUser = new JwtUser(readUser.getId(),
+                readUser.getFirstName(),
+                readUser.getLastName(),
+                readUser.getEmail(),
+                readUser.getRole(),
+                readUser.getPassword());
+
+        return jwtUser;
+    }
+}
