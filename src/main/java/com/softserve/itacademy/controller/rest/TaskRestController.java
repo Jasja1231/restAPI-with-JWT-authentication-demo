76202d660b260500e36f7918694c7528c1471632
Vladimir Zinchenko <Zincvalo5@gmail.com>
2022-11-08 21:17:29 +0200

    Update TaskRestController

--- src/main/java/com/softserve/itacademy/controller/rest/TaskRestController.java
+++ src/main/java/com/softserve/itacademy/controller/rest/TaskRestController.java
@@ -11,6 +11,7 @@ import com.softserve.itacademy.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
@@ -40,6 +41,7 @@ List<TaskDto> getAll(){
 TaskDto getById(@PathVariable long id){return TaskTransformer.convertToDto(taskService.readById(id));}
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN') or @toDoServiceImpl.canAccessToDo(#todoId)")
     @ResponseStatus(HttpStatus.CREATED)
     ResponseEntity<?>create(@RequestBody TaskDto taskDto){
         Task task = new Task();
@@ -58,6 +60,7 @@ TaskDto getById(@PathVariable long id){return TaskTransformer.convertToDto(taskS
 
 }
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or @toDoServiceImpl.canAccessToDo(#todoId)")
     @ResponseStatus(HttpStatus.CREATED)
     ResponseEntity<Object> put(@PathVariable long id,@RequestBody TaskDto taskDto) {
 
@@ -66,7 +69,8 @@ TaskDto getById(@PathVariable long id){return TaskTransformer.convertToDto(taskS
         task.setPriority(Priority.valueOf(taskDto.getPriority()));
         task.setTodo(toDoService.readById(taskDto.getTodoId()));
         task.setState(stateService.readById(taskDto.getStateId()));
-        taskService.create(task);
+        taskService.update(task);
+
 
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
@@ -76,6 +80,7 @@ TaskDto getById(@PathVariable long id){return TaskTransformer.convertToDto(taskS
         return ResponseEntity.created(location).build();
     }
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or @toDoServiceImpl.canAccessToDo(#todoId)")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     void delete(@PathVariable long id) {
         taskService.delete(id);
